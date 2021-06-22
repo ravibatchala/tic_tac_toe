ae26d4a583b511177881f81d53db2d98e45fe204
ravibatchala <batchalaraviteja@gmail.com>
2021-06-22 22:07:02 +0530

    commited the Sqaure.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Square = props => {
+const Square = ({ value }) => {
   console.log(props);
-  return <button type="button">{props.value}</button>;
+  return <button type="button">{value}</button>;
 };
 
 export default Square;
